953ae3e5acfc7660cef6e2293cacc9b5aca5cab4
Fang Peng <support-v-fapeng@hotmail.com>
2016-06-21 13:15:41 +0800

    Add files via upload

---
+++ Samples/X-Bind/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace X_Bind
+{
+    public class test
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+    }
+
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public List<test> testlist { get; set; }
+        public ViewModel myviewmode { get; set; }
+        public MainPage()
+        {
+            this.InitializeComponent();
+            testlist = new List<test>();
+            testlist.Add(new test() { id="test",name="name1"});
+            testlist.Add(new test() { id = "test1", name = "name1" });
+            testlist.Add(new test() { id = "test2", name = "name1" });
+            testlist.Add(new test() { id = "test3", name = "name1" });
+
+            myviewmode = new ViewModel();
+        }
+    }
+}
---
+++ Samples/X-Bind/ViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace X_Bind
+{
+    public class test1
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+    }
+  public class ViewModel
+    {
+        public List<test1> testlist1 { get; set; }
+        public ViewModel()
+        {
+            testlist1 = new List<test1>();
+            testlist1.Add(new test1() { id = "test", name = "name1" });
+            testlist1.Add(new test1() { id = "test1", name = "name1" });
+            testlist1.Add(new test1() { id = "test2", name = "name1" });
+            testlist1.Add(new test1() { id = "test3", name = "name1" });
+        }
+     
+    }
+}
